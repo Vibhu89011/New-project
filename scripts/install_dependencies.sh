39495288854646bb95fab76d1af6467adb5d06df
Vibhu89011 <128780898+Vibhu89011@users.noreply.github.com>
2023-04-12 15:53:10 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,7 @@
 #Bash script to install Apache httpd Server in Enterprise Linux 7.x
 #Run this script as root
 
-set-e  # stop script execution on any error
+set -e  # stop script execution on any error 
+
 #Install Apache httpd
 yum -y install httpd
